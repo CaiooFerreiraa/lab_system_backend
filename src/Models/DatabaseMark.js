92bf0204dcf19195776c77d79bad53d9ece16833
CaiooFerreiaa <caioferreiraadev@gmail.com>
2025-10-03 22:36:30 -0300

    update: Refatorando funções

--- src/Models/DatabaseMark.js
+++ src/Models/DatabaseMark.js
@@ -1,5 +1,19 @@
 import dataBase from '../../bd.js'
 
 export default class DatabaseMark {
+  createMark() {
 
+  }
+
+  readMark() {
+
+  }
+
+  updateMark() {
+
+  }
+
+  deleteMark() {
+    
+  }
 }
