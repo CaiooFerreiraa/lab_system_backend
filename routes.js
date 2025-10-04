8020567887e85d000808851020450a23ad3760e1
CaiooFerreiaa <caioferreiraadev@gmail.com>
2025-10-04 18:59:03 -0300

    update: Finalizando CRUD backend de funcionário

--- routes.js
+++ routes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import func from './src/controllers/FuncionarioController.js';
+import managerEmployees from './src/controllers/FuncionarioController.js';
 const route = Router();
 
 route.get('/', (req, res) => {res.sendStatus(200)});
-route.get('/register', func.registerEmployee);
-route.get('/view', func.viewsEmployee);
-route.get('/update', func.updateEmployee);
-route.get('/delete', func.deleteEmployee)
+route.get('/register', managerEmployees.registerEmployee);
+route.get('/view', managerEmployees.viewsEmployee);
+route.get('/update', managerEmployees.updateEmployee);
+route.get('/delete', managerEmployees.deleteEmployee)
 
 export default route;
--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const port = process.env.PORT;
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+// Aqui ficara o middleware de verificação se é o admin do sistema que está fazendo essas inserções
 app.use(route)
 
 app.listen(port, () => console.log("Servidor rodando"))
